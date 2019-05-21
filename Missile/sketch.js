7ca6820407333358b181cd3002f120f660c40033
Ahmet Akif Kaya <ahmetakifkaya11@gmail.com>
2019-05-21 23:39:34 +0300

    Update sketch.js

--- Missile/sketch.js
+++ Missile/sketch.js
@@ -287,3 +287,21 @@ function drawbackground() {
   }
   pop();
 }
+
+function onDeviceReady() {
+    document.removeEventListener('deviceready', onDeviceReady, false);
+
+    // Set AdMobAds options:
+    admob.setOptions({
+        publisherId:          "ca-app-pub-3940256099942544/6300978111",  // Required
+        interstitialAdId:     "ca-app-pub-XXXXXXXXXXXXXXXX/IIIIIIIIII",  // Optional
+        tappxIdiOS:           "/XXXXXXXXX/Pub-XXXX-iOS-IIII",            // Optional
+        tappxIdAndroid:       "/XXXXXXXXX/Pub-XXXX-Android-AAAA",        // Optional
+        tappxShare:           0.5                                        // Optional
+    });
+
+    admob.createBannerView();
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
